da9fe30d39623ec274ca08599ea1eb6d14b5a420
Juanpa8830 <jp3_4@hotmail.com>
2023-05-11 00:29:18 -0500

    Configure app global store

---
+++ src/Redux/ConfigureStore.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+//import reducers:
+import MessagesReducer from './slices/MessagesReducer';
+
+const rootReducer = {
+    Messages: MessagesReducer,
+  };
+  
+  const store = configureStore({ reducer: rootReducer });// la nueva sintaxis de configureStore
+  
+  export default store;
