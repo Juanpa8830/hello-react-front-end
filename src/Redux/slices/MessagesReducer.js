f996a5910844bc12f09a77a2bf948dd9c5bfae9d
Juanpa8830 <jp3_4@hotmail.com>
2023-05-11 00:29:50 -0500

    Add reducer to for random messages from rails API

---
+++ src/Redux/slices/MessagesReducer.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const GET_DATA = 'GET_DATA';
+const messagesUrl = 'http://localhost:3000/api/v1/messages';
+
+const MessagesReducer = (state = [], action) => {
+    switch (action.type) {
+    case `${GET_DATA}/fulfilled`:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const getApiMessages = createAsyncThunk(GET_DATA,
+  () => axios.get(messagesUrl).then((response) => {
+       const MessagesObj = response.data
+    
+      return MessagesObj;
+  }));
+
+export default MessagesReducer;
