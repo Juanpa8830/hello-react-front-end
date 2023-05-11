1300bab5ba14768a6932541f036e9e0b87808e51
Juanpa8830 <jp3_4@hotmail.com>
2023-05-11 00:29:00 -0500

    Add browser router and store to app component

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './Redux/ConfigureStore';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+     </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
